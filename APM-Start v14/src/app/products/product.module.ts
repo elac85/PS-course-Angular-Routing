e89bee1a60821e01dbe2b8f8fbac5c733432c7f7
elac85 <enekes.laszlo85@gmail.com>
2022-11-14 09:42:06 +0100

    lesson 7 child routes start

--- APM-Start v14/src/app/products/product.module.ts
+++ APM-Start v14/src/app/products/product.module.ts
@@ -1,30 +1,52 @@
-import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule} from '@angular/router';
 
-import { ProductListComponent } from './product-list.component';
-import { ProductDetailComponent } from './product-detail.component';
-import { ProductEditComponent } from './product-edit/product-edit.component';
-import { ProductSearchComponent } from './product-search/product-search.component';
+import {ProductListComponent} from './product-list.component';
+import {ProductDetailComponent} from './product-detail.component';
+import {ProductEditComponent} from './product-edit/product-edit.component';
+import {ProductSearchComponent} from './product-search/product-search.component';
 
-import { SharedModule } from '../shared/shared.module';
-import { ProductResolver } from './product-resolver.service';
-import { ProductsResolver } from './products-resolver.service';
+import {SharedModule} from '../shared/shared.module';
+import {ProductResolver} from './product-resolver.service';
+import {ProductsResolver} from './products-resolver.service';
+import {ProductEditInfoComponent} from "./product-edit/product-edit-info.component";
+import {ProductEditTagsComponent} from "./product-edit/product-edit-tags.component";
 
 @NgModule({
   imports: [
     SharedModule,
     RouterModule.forChild([
-      { path: 'products', component: ProductListComponent, resolve: { resolvedData: ProductsResolver } },
-      { path: 'products/search', component: ProductSearchComponent },
-      { path: 'products/:id', component: ProductDetailComponent, resolve: { resolvedData: ProductResolver } },
-      { path: 'products/:id/edit', component: ProductEditComponent, resolve: { resolvedData: ProductResolver } }
+      {path: 'products', component: ProductListComponent, resolve: {resolvedData: ProductsResolver}},
+      {path: 'products/search', component: ProductSearchComponent},
+      {path: 'products/:id', component: ProductDetailComponent, resolve: {resolvedData: ProductResolver}},
+      {
+        path: 'products/:id/edit', component: ProductEditComponent, resolve: {resolvedData: ProductResolver},
+        children: [
+          {
+            path: '',
+            redirectTo: 'info',
+            pathMatch: "full"
+          },
+          {
+            path: 'info',
+            component: ProductEditInfoComponent
+          },
+          {
+            path: 'tags',
+            component: ProductEditTagsComponent
+          }
+        ]
+      }
     ])
   ],
   declarations: [
     ProductListComponent,
     ProductDetailComponent,
     ProductEditComponent,
-    ProductSearchComponent
+    ProductSearchComponent,
+    ProductEditInfoComponent,
+    ProductEditTagsComponent
   ]
 })
-export class ProductModule { }
+export class ProductModule {
+}
